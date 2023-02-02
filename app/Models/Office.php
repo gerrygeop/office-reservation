dcdd2d31072ea21ff2e04fdfdafe78539352112e
gerrygeop <gerryja28@gmail.com>
2023-02-02 23:36:05 +0800

    set migration & factory

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
+class Office extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
-
-    public $timestamps = false;
 }
--- database/migrations/2023_02_02_150604_create_images_table.php
+++ database/migrations/2023_02_02_150604_create_images_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->morphs('resource');
+            $table->string('path');
+            $table->timestamps();
         });
-
-        Tag::create(['name' => 'has_ac']);
-        Tag::create(['name' => 'has_private_bathroom']);
-        Tag::create(['name' => 'has_coffee_machine']);
     }
 
     /**
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('images');
     }
 };
