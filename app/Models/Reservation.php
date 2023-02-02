b9339ad7a16f6b0b614ae31edd33ebd3a06b5f22
gerrygeop <gerryja28@gmail.com>
2023-02-02 23:59:26 +0800

    setup model & factory

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
     use HasFactory;
 
+    const STATUS_ACTIVE = 1;
+    const STATUS_CANCEL = 2;
+
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function office(): BelongsTo
+    {
+        return $this->belongsTo(Office::class);
+    }
 }
